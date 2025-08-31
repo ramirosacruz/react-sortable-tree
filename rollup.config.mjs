c92f242141242bb08209a624e5394b4a8a3553a0
Ramiro <ramirosacruz@gmail.com>
2025-08-31 16:29:12 +0000

    a

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,36 +3,38 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import postcss from "rollup-plugin-postcss";
 import dts from "rollup-plugin-dts";
-
-// This is required to read package.json file when
-// using Native ES modules in Node.js
-// https://rollupjs.org/command-line-interface/#importing-package-json
 import { createRequire } from 'node:module';
+
 const requireFile = createRequire(import.meta.url);
 const packageJson = requireFile('./package.json');
 
-
-export default [{
-  input: "src/index.ts",
-  output: [
-    {
-      file: packageJson.module,
-      format: "esm",
-      sourcemap: true
-    }
-  ],
-  external: [/node_modules/],
-  plugins: [
-    resolve(),
-    commonjs(),
-    typescript(),
-    postcss({
-      extensions: ['.css']
-    }),
-  ]
-}, {
-  input: 'lib/index.d.ts',
-  output: [{ file: 'lib/index.d.ts', format: 'es' }],
-  plugins: [dts()],
-  external: [/\.css$/]
-}];
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true
+      }
+    ],
+    external: [
+      ...Object.keys(packageJson.peerDependencies || {})
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript(),
+      postcss({
+        extensions: ['.css'],
+        inject: true
+      }),
+    ]
+  },
+  {
+    input: 'src/index.ts',
+    output: [{ file: 'lib/index.d.ts', format: 'es' }],
+    plugins: [dts()],
+    external: [/\.css$/]
+  }
+];
